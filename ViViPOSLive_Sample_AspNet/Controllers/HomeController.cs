db712fe4595a27ebbaed4bfd29e8760a95ca3e03
ViViPOS Developer <developer@vivipos.com.au>
2014-05-04 13:16:25 +1000

    Add some comments to source code

--- ViViPOSLive_Sample_AspNet/Controllers/HomeController.cs
+++ ViViPOSLive_Sample_AspNet/Controllers/HomeController.cs
@@ -15,18 +15,21 @@ namespace ViViPOSLive_Sample_AspNet.Controllers
 {
     public class HomeController : Controller
     {
-
+        //Wait Handle
         AutoResetEvent waitHandle = new AutoResetEvent(false);
+
         public ActionResult Index()
         {
             Product[] products = null;
+
             var connection = new HubConnection(ConfigurationManager.AppSettings["viviposliveURL"],
                 "app_id=" + ConfigurationManager.AppSettings["app_id"] +
                 "&app_key=" + ConfigurationManager.AppSettings["app_key"] +
                 "&cid=" + ConfigurationManager.AppSettings["cid"] +
                 "&s=" + ConfigurationManager.AppSettings["s"]);
 
-            var proxy = connection.CreateHubProxy("LiveHub");
+            var proxy = connection.CreateHubProxy("LiveHub");   //Create Proxy for ViViPOS Live Hub
+
             bool connected = false;
             connection.Start().ContinueWith(task =>
             {
@@ -43,27 +46,32 @@ namespace ViViPOSLive_Sample_AspNet.Controllers
 
             if (connected)
             {
-                var request = new Request()
-                {
-                    meta = new Meta() { target = "aabbccddeeff" },
-                    data = null
-                };
 
+                //Setup onError handler
                 proxy.On<string>("onError", (errorMessage) =>
                 {
-                    ViewBag.ErrorMessage = errorMessage;
-                    waitHandle.Set();
-                }
-                );
-
-                proxy.On<Response>("onProductsReceived", (response) =>
-                {
-                    products = JsonConvert.DeserializeObject<Product[]>(response.data.ToString());
-
+                    ViewBag.ErrorMessage = errorMessage;    //Set error Message to display if there is an error
                     waitHandle.Set();
                 });
 
 
+                //Set up call back handler when product json is received from ViViPOS terminal
+                proxy.On<Response>("onProductsReceived", (response) =>
+                {
+                    products = JsonConvert.DeserializeObject<Product[]>(response.data.ToString()); 
+                    waitHandle.Set();
+                });
+
+
+                var request = new Request()
+                {
+                    //Replace Target with the MacAddress of the targetted ViViPOS 
+                    meta = new Meta() { target = "aabbccddeeff" },
+                    data = null
+                };
+
+
+                //Try to get products from ViViPOS terminal, pass in request object
                 proxy.Invoke<string>("GetProducts", request).ContinueWith(task =>
                 {
                     if (task.IsFaulted)
@@ -77,29 +85,36 @@ namespace ViViPOSLive_Sample_AspNet.Controllers
                 });
 
 
-                waitHandle.WaitOne();
+                waitHandle.WaitOne();   //Wait until we receive messages from the ViViPOS
 
-                return View(products);
+                return View(products);  //return results to the View
             }
             else
             {
+                //If there is a connection error, display error
                 ViewBag.ErrorMessage = "Error Connecting to ViViPOS Live";
                 return View(products);
             }
 
         }
 
-
+        /// <summary>
+        /// Dummy About Page
+        /// </summary>
+        /// <returns></returns>
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
             return View();
         }
 
+        /// <summary>
+        /// Dummy Contact Page
+        /// </summary>
+        /// <returns></returns>
         public ActionResult Contact()
         {
             ViewBag.Message = "ViViPOS Australia";
-
             return View();
         }
     }
